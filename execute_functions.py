bd676256e898932fe359689b9209296995ee19b1
dipti08 <62476637+dipti08@users.noreply.github.com>
2020-06-18 15:51:00 +0530

    execute_functions

---
+++ execute_functions.py
@@ -0,0 +1,16 @@
+#importing the necessary libraries and functions
+from lib_dipti import read_file
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+from lib_dipti import copy_from_one_file_to_another
+#from lib_dipti import read_pdf
+#from lib_dipti import pdf_copy
+
+read_file()
+copy_from_one_file_to_another()
+#read_pdf()
+#pdf_copy()
