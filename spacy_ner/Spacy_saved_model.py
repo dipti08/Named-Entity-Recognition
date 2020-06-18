6ded79d56cbbe39ef4b23a4e5b0ca3b69e12f38f
Dipti Agarwal <62476637+dipti08@users.noreply.github.com>
2020-06-18 18:29:53 +0530

    updated spacy_saved_model

--- spacy_ner/Spacy_saved_model.py
+++ spacy_ner/Spacy_saved_model.py
@@ -4,11 +4,15 @@
 # Training additional entity types using spaCy
 from __future__ import unicode_literals, print_function
 import pickle
+import nltk
 import plac
 import random
 from pathlib import Path
 import spacy
+import unidecode
 from spacy.util import minibatch, compounding
+from nltk.tokenize import sent_tokenize
+from tika import parser
 
 # Loading training data
 
@@ -23,13 +27,34 @@ def main(model=None, new_model_name='new_model', output_dir=r'C:\Users\DIPTI AGA
 
     print("Loading from", output_dir)
     nlp2 = spacy.load(output_dir)
-    test = 'HPE use Python, MongoDB, Node.js and JSON for developing NLP and Machine Learning applications for phone and Tablet '
-    print(test)
-    doc2 = nlp2(test)
-    for ent in doc2.ents:
-        print(ent.label_, ent.text)
+    #test = 'HPE use Python, MongoDB, Node.js and JSON for developing NLP and Machine Learning applications for phone and Tablet '
+    #print(test)
 
+    # f=open(r'C:\Users\DIPTI AGARWAL\Desktop\robot\spacy_ner\mydocu.txt')
+    # contents=f.read()
+    # sentences=sent_tokenize(contents)
+    # for sentence in sentences:
+    #     test = sentence
+    #     doc2 = nlp2(test)
+    #     for ent in doc2.ents:
+    #         print(ent.label_, ent.text)
+
+    filename = r'C:\Users\DIPTI AGARWAL\Desktop\robot\spacy_ner\mydocu.pdf'
+    parsedPDF = parser.from_file(filename)
+    pdf = parsedPDF["content"]
+    pdf = pdf.replace('\n\n', '')
+    print(pdf)
+    data = nltk.tokenize.sent_tokenize(pdf)
+    print(data)
+
+    print("Loading from", output_dir)
+    nlp2 = spacy.load(output_dir)
+    for test in data:
+        test_ner = test
+        print(test_ner)
+        doc2 = nlp2(test_ner)
+        for ent in doc2.ents:
+            print(ent.label_, ent.text)
 
 if __name__ == '__main__':
     plac.call(main)
-    
