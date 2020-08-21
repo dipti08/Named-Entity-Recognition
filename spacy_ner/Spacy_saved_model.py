0b07d5f725ec29d5955ef858fa64fff923cf7b30
Dipti Agarwal <62476637+dipti08@users.noreply.github.com>
2020-08-21 17:28:28 +0530

    Update Spacy_saved_model.py

--- spacy_ner/Spacy_saved_model.py
+++ spacy_ner/Spacy_saved_model.py
@@ -9,12 +9,9 @@ import plac
 import random
 from pathlib import Path
 import spacy
-import unidecode
 from spacy.util import minibatch, compounding
-from nltk.tokenize import sent_tokenize
+import nltk
 from tika import parser
-
-# Loading training data
 
 
 @plac.annotations(
@@ -22,24 +19,9 @@ from tika import parser
     new_model_name=("New model name for model meta.", "option", "nm", str),
     output_dir=("Optional output directory", "option", "o", Path),
     n_iter=("Number of training iterations", "option", "n", int))
-def main(model=None, new_model_name='new_model', output_dir=r'C:\Users\DIPTI AGARWAL\Desktop\robot\spacy_ner\spacy saved model', n_iter=6):
-    """Setting up the pipeline and entity recognizer, and training the new entity."""
-
-    print("Loading from", output_dir)
-    nlp2 = spacy.load(output_dir)
-    #test = 'HPE use Python, MongoDB, Node.js and JSON for developing NLP and Machine Learning applications for phone and Tablet '
-    #print(test)
-
-    # f=open(r'C:\Users\DIPTI AGARWAL\Desktop\robot\spacy_ner\mydocu.txt')
-    # contents=f.read()
-    # sentences=sent_tokenize(contents)
-    # for sentence in sentences:
-    #     test = sentence
-    #     doc2 = nlp2(test)
-    #     for ent in doc2.ents:
-    #         print(ent.label_, ent.text)
-
-    filename = r'C:\Users\DIPTI AGARWAL\Desktop\robot\spacy_ner\mydocu.pdf'
+def main(model=None, new_model_name='new_model', output_dir=r'C:\Users\Ayushi\Desktop\Ayushi\HPE\spacy saved model',
+         n_iter=6):
+    filename = r'C:\Users\Ayushi\Desktop\Ayushi\HPE\\HPE.pdf'
     parsedPDF = parser.from_file(filename)
     pdf = parsedPDF["content"]
     pdf = pdf.replace('\n\n', '')
@@ -55,6 +37,7 @@ def main(model=None, new_model_name='new_model', output_dir=r'C:\Users\DIPTI AGA
         doc2 = nlp2(test_ner)
         for ent in doc2.ents:
             print(ent.label_, ent.text)
+
 
 if __name__ == '__main__':
     plac.call(main)
